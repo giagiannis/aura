092bbefff1583d2acb33156bbe8da210dfcded11
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2017-05-10 00:11:18 +0300

    added test

---
+++ tests/test_appdescription.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import sys
+import random
+import unittest
+from aura.core import ApplicationDescriptionParser
+
+
+class TestDescription(unittest.TestCase):
+    def test_foo(self):
+        desc = ApplicationDescriptionParser("example/demo")
+        multi = {}
+        for m in desc.get_description()['modules']:
+            multi[m['name']] = random.randint(1,9)
+        desc.set_multiplicities(multi)
+        expanded = desc.expand_description()
+        self.assertEqual(sum(multi.values()), len(expanded['modules']))
+        result = {}
+        for m in expanded['modules']:
+            pref = m['name'][0:len(m['name'])-1]
+            if pref not in result:
+                result[pref]=0
+            result[pref]+=1
+        for key in result:
+            self.assertEqual(result[key], multi[key])
+
